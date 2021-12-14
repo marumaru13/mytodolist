31b57da2218f3e2321f336c384d483241c3dc300
mai yamamaru <for.life0311@gmail.com>
2021-12-14 16:42:28 +0900

    refrashしても残る

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
+let parent = document.getElementsByClassName("todo-list");
+let finishoya=document.getElementsByClassName('finishoya');
+
 var data=JSON.parse(localStorage.getItem('key'));
+var data2=JSON.parse(localStorage.getItem('key2'));
+// readingのif文
 if (data==null){
   data=[];
 }else{
   for(var i=0;i<data.length;i++){
-    creatTask(data[i]);
+    creatTask(data[i],parent);
  };
 }
-// let code=JSON.parse(text);
+// finishedのif分
+if (data2==null){
+  data2=[];
+}else{
+  for(var i=0;i<data2.length;i++){
+    creatTask(data2[i],finishoya);
+ };
+}
+
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click',function(){
@@ -14,57 +27,22 @@ btn.addEventListener('click',function(){
     const task = input.value;
     
     if(task!=''){
-      creatTask(task);
+      creatTask(task,parent);
       data.push(task);
       let text = JSON.stringify(data);
       // ローカルストレージに保存
       localStorage.setItem('key',text);
-
-  //  let li = document.createElement('li');
-
-  //  li.classList.add('list');
-
-  //  li.textContent = task;
-
-  //  let parent = document.getElementsByClassName("todo-list");
-  //  parent[0].appendChild(li);
-   
-  //  let trash = document.createElement('div');
-  //  trash.textContent = 'Delete';
-  //  trash.classList.add('delete');
-   
-  //  let finish = document.createElement('div');
-  //  let oya1=document.createElement('div');
-  //  oya1.classList.add('oya');
-
-  //  finish.textContent ='Done';
-  // //  console.log(finish);
-  //  finish.classList.add('finish')
-  //  oya1.appendChild(finish);
-  //  oya1.appendChild(trash);
-  //  li.appendChild(oya1);
-
-  //  finish.addEventListener('click',function(){
-  //   // this.parentElement.remove();
-  //   let finishoya=document.getElementsByClassName('finishoya')
-  //   console.log(finishoya);
-  //   this.remove();
-  //   finishoya[0].appendChild(li);
-  //  });
-   
-   
   };
 });
-
-function creatTask(karabako){
+// 関数を作る
+function creatTask(karabako,karabako2){
   let li = document.createElement('li');
 
   li.classList.add('list');
 
   li.textContent = karabako;
 
-  let parent = document.getElementsByClassName("todo-list");
-  parent[0].appendChild(li);
+  karabako2[0].appendChild(li);
   // ゴミ箱の要素を作る
   let trash = document.createElement('div');
   trash.textContent = 'Delete'; 
@@ -83,25 +61,45 @@ function creatTask(karabako){
 
   finish.addEventListener('click',function(){
     // this.parentElement.remove();
-    let finishoya=document.getElementsByClassName('finishoya')
-    console.log(finishoya);
+    console.log(karabako2);
     this.remove();
     finishoya[0].appendChild(li);
 
-    //  配列から消す
-    // indexOf()で何番目か特定
-    let res=data.indexOf(karabako);
+    data2.push(karabako);
+    console.log(data2);
     
-    data.splice(data.indexOf(karabako));
-
-    let text = JSON.stringify(data);
+    text = JSON.stringify(data2);
     // ローカルストレージに保存
-    localStorage.setItem('key',text);    
+    localStorage.setItem('key2',text);   
+    // readingに入ったのを消す
+    data.splice(data.indexOf(karabako));
+       text = JSON.stringify(data);
+    // ローカルストレージに保存
+    localStorage.setItem('key',text);   
   });
   
   trash.addEventListener('click',function(){
+    let judge = this.closest(".finishoya");
+    // finishが親じゃない時nullになる
     this.parentElement.parentElement.remove();
-   // console.log(this.parentElement.parentElement);
+
+   //  配列から消す
+   // indexOf()で何番目か特定
+    let text='';
+    let res=data.indexOf(karabako);
+
+    // 消してね
+    if(judge == null){
+      data.splice(data.indexOf(karabako));
+      let text = JSON.stringify(data);
+      // ローカルストレージに保存
+      localStorage.setItem('key',text);   
+    }else{
+      data2.splice(data2.indexOf(karabako));
+      text = JSON.stringify(data2);
+      // ローカルストレージに保存
+      localStorage.setItem('key2',text);     
+    }
   });
 
 };
